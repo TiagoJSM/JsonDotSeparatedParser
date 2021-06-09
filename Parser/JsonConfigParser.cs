025f15d174b78c659739c101cf5e062dc8b3df24
Tiago Martins <tiago_gobackto80s@hotmail.com>
2021-06-10 00:02:44 +0200

    Add documentation comments.

--- Parser/JsonConfigParser.cs
+++ Parser/JsonConfigParser.cs
@@ -1,12 +1,16 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace Parser
 {
     public class JsonConfigParser
     {
+        /// <summary>
+        /// Parses json config files, when providing more than 1 parameter the files are merged.
+        /// </summary>
+        /// <param name="filePaths">The path for the json files.</param>
+        /// <returns></returns>
         public dynamic ParseConfigFiles(params string[] filePaths)
         {
             var resultConfig = new JObject();
@@ -29,7 +33,7 @@ namespace Parser
             }
             var jsonText = File.ReadAllText(filePath);
 
-            // Newtonsoft Json doesn't private a method to check if a parse is possible, so for now we have to use a try-catch to avoid exceptions bubbling up
+            // Newtonsoft Json doesn't provide a method to check if a parse is possible, so for now we have to use a try-catch to avoid exceptions bubbling up
             // ToDo: Replace try-catch if Newtonsoft Json provides a "validation" method
             try
             {
