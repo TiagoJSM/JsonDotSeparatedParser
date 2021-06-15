c9a2c40eff5caf835405e389e3deed516abe62a9
Tiago Martins <tiago_gobackto80s@hotmail.com>
2021-06-15 19:20:00 +0200

    Improve main method information. Improve README.md

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -7,9 +7,21 @@ namespace Parser
         static void Main(string[] args)
         {
             var parser = new JsonConfigParser();
+            var path = "database.host";
+            var configFiles = new[] { "fixtures/config.json", "fixtures/config.local.json" };
 
-            var config = parser.ParseConfigFiles("fixtures/config.json", "fixtures/config.local.json");
-            var data = JsonDotSeparatedPath.Select<string>(config, "database.host");
+            var config = parser.ParseConfigFiles(configFiles);
+            var data = JsonDotSeparatedPath.Select<string>(config, path);
+
+            Console.WriteLine("Config files loaded:");
+            foreach (var configFile in configFiles)
+            {
+                Console.WriteLine(configFile);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Dot Separated Path: {path}");
+            Console.WriteLine($"Result: {data}");
         }
     }
 }
